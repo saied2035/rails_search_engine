b397ab555645d6473c75d9523f3458a62134b1b9
saied2035 <saied2421998@gmail.com>
2024-01-04 20:31:04 +0200

    finish recomendation//search logic

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,68 @@
 class SearchController < ApplicationController
-  def index
+  def index 
   end
+
+  def search
+    @books = Book.search(params[:search], operator: 'or')
+    @recomendations = @books.length > 0 ? analytics : []
+    puts "recomendations: #{@recomendations[0]}"
+    render turbo_stream: [
+      turbo_stream.update('books', partial: 'results', locals: {books: @books }),
+      turbo_stream.update('recomendations', partial: 'recomendations',
+      locals: {recomendations: @recomendations })
+    ] 
+  end
+  
+  def analytics
+    genres = {}
+    max_genre = []
+    publishers = {}
+    max_publisher =  []
+    authors = {}
+    max_author = []
+    
+    @books.includes(:genre, :publisher, :author).each do |book|
+
+      genres["#{book.genre.name}"] = genres["#{book.genre.name}"].nil? ? 
+      { ids: [book.id], repeat: 1 } 
+      :
+      { ids: genres["#{book.genre.name}"][:ids].push(book.id) , 
+      repeat: genres["#{book.genre.name}"][:repeat] + 1 }
+
+      max_genre[genres["#{book.genre.name}"][:repeat]] = book.genre
+
+      publishers["#{book.publisher.name}"] = publishers["#{book.publisher.name}"].nil? ? 
+      { ids: [book.id], repeat: 1 } 
+      :
+      { ids: publishers["#{book.publisher.name}"][:ids].push(book.id) ,
+      repeat: publishers["#{book.publisher.name}"][:repeat] + 1 }
+
+      max_publisher[publishers["#{book.publisher.name}"][:repeat]] = book.publisher
+
+      authors["#{book.author.name}"] = authors["#{book.author.name}"].nil? ? 
+      { ids: [book.id], repeat: 1 } 
+      :
+      { ids: authors["#{book.author.name}"][:ids].push(book.id) ,
+      repeat: authors["#{book.author.name}"][:repeat] + 1 }
+
+      max_author[authors["#{book.author.name}"][:repeat]] = book.author
+    end
+
+    author = max_author[max_author.length - 1]
+    genre = max_genre[max_genre.length - 1]
+    publisher = max_publisher[max_publisher.length - 1]
+
+    author_books = Book.where(author: author).
+    where.not(id: authors["#{author.name}"][:ids])
+
+    genre_books = Book.where(genre: genre).
+    where.not(id: genres["#{genre.name}"][:ids])
+
+    publisher_books = Book.where(publisher: publisher).
+    where.not(id: publishers["#{publisher.name}"][:ids])
+    publisher_books.each {|book| puts book}
+    max_arr = [author_books.length, genre_books.length, publisher_books.length]
+    max_index = max_arr.index(max_arr.max)
+    return max_index == 0 ? author_books : max_arr == 1 ? genre_books : publisher_books
+  end  
 end
--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -6,6 +6,27 @@ export default class extends Controller {
   connect() {
   }
   searchProcess() {
-    console.log('detect action', this.inputTarget.value);
+    const booksSection = document.querySelector('#books')
+    booksSection.classList.add('hidden');
+    clearTimeout(this.timeout)
+    this.timeout =  setTimeout(() => {
+      const input = this.inputTarget.value;
+      const url = this.element.dataset.requestUrl
+      const csrf = document.querySelector("meta[name='csrf-token']").getAttribute("content");
+      const options = {
+        method: "POST",
+        headers: {
+          'Content-Type': "application/json",
+          'X-CSRF-Token': csrf
+        },
+        body: JSON.stringify({ search: input })
+      }
+
+      fetch(url, options).then(res =>res.text())
+      .then(res => {
+        booksSection.classList.remove('hidden')
+        document.body.insertAdjacentHTML('beforeend', res)
+      })
+    }, 1500) 
   }
 }
